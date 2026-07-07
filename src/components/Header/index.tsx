573f6c26473d34c208ccf107d6cc161d0e1ee8c0
matos795 <matosalexandre795@gmail.com>
2026-07-07 15:56:06 -0300

    feat: Header Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import "./styles.css"
+
+export default function Header() {
+    return (
+        <>
+            <header className="dsf-header">
+                <nav className="dsf-container">
+                        <h1>DSFilter</h1>
+                    <div className="dsf-nav-right">
+                        6 produto(s)
+                    </div>
+                </nav>
+            </header>
+        </>
+    )
+}
